05e1ca08839ce814989288abb1ad44697f63a4f5
Olivier Dagenais <olivier.dagenais@gmail.com>
2016-12-31 22:02:16 -0500

    Initial POM and skeleton Program[Test]

---
+++ src/main/groovy/ca/bravodelta/kodimata/Program.groovy
@@ -0,0 +1,17 @@
+// Copyright (c) Olivier Dagenais. All rights reserved.
+// Licensed under the MIT license. See LICENSE in the project root.
+
+package ca.bravodelta.kodimata
+
+import groovy.transform.CompileStatic
+
+/**
+ * Main entry point
+ */
+@CompileStatic
+public class Program {
+
+    public static void main(final String[] args) {
+    }
+
+}
---
+++ src/test/groovy/ca/bravodelta/kodimata/ProgramTest.groovy
@@ -0,0 +1,19 @@
+// Copyright (c) Olivier Dagenais. All rights reserved.
+// Licensed under the MIT license. See LICENSE in the project root.
+
+package ca.bravodelta.kodimata
+
+import groovy.transform.CompileStatic
+import org.junit.Test
+
+/**
+ * A class to test {@link Program}.
+ */
+@CompileStatic
+public class ProgramTest {
+
+    @Test public void main_doesNotThrow() {
+        Program.main();
+    }
+
+}
