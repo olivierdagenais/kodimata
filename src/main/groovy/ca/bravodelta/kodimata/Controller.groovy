16422479ef7ab482acdfbf746b3b42549d3e4f72
Olivier Dagenais <olivier.dagenais@gmail.com>
2017-01-02 23:17:06 -0500

    Introducing skeleton Controller

---
+++ src/main/groovy/ca/bravodelta/kodimata/Controller.groovy
@@ -0,0 +1,15 @@
+// Copyright (c) Olivier Dagenais. All rights reserved.
+// Licensed under the MIT license. See LICENSE in the project root.
+
+package ca.bravodelta.kodimata
+
+public class Controller {
+
+    public void configureButton(final int pinId, final Closure action) {
+        // TODO: Record button configuration
+    }
+
+    public void buttonPressed(final int pinId) {
+        // TODO: execute corresponding action
+    }
+}
---
+++ src/test/groovy/ca/bravodelta/kodimata/ControllerTest.groovy
@@ -0,0 +1,15 @@
+// Copyright (c) Olivier Dagenais. All rights reserved.
+// Licensed under the MIT license. See LICENSE in the project root.
+
+package ca.bravodelta.kodimata
+
+import groovy.transform.CompileStatic
+import org.junit.Test
+
+/**
+ * A class to test {@link Controller}.
+ */
+@CompileStatic
+public class ControllerTest {
+
+}
